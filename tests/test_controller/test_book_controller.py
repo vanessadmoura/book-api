acdad158aacdc6b4719be9020b3d00dd9c03aad8
vanessadmoura <nessa.moura8@gmail.com>
2023-02-20 17:04:20 -0300

    Add mocks in tests using mocker and magicmock

--- tests/test_controller/test_book_controller.py
+++ tests/test_controller/test_book_controller.py
@@ -1,31 +1,55 @@
-from tests.utils.create_book import create_valid_list_book
+from tests.utils.create_book import create_response_successful_generate, create_valid_book, create_valid_list_book
 
 
 def test_get_all_books(client, mocker):
     list_books_mock = mocker.MagicMock(return_value=create_valid_list_book())
-    mocker.patch("controller.books.get_all_book", create_valid_list_book())
+    mocker.patch("controller.book_controller.get_all_book", list_books_mock)
 
     response = client.get("/book")
 
     assert response.status_code == 200
     assert len(response.json) == 2
+
     
-def test_get_book_by_id(client):
+def test_get_book_by_id(client, mocker):
+    list_book_mock = mocker.MagicMock(return_value=create_valid_book())
+    mocker.patch("controller.book_controller.get_book_by_id", list_book_mock)
+
     response = client.get("/book/1")
+
     assert response.status_code == 200
+    assert len(response.json) == 1
 
-def test_register_book(client):
-    body = {"livro": "Python",
-            "escritor": "Igor"}
+
+def test_register_book(client, mocker):
+    body = {"livro": "any_value",
+            "escritor": "any_value"}
+    register_book_mock = mocker.MagicMock()
+    mocker.patch("controller.book_controller.register_book", register_book_mock)
+    
     response = client.post("/book", json=body)
-    assert response.status_code == 201
 
-def test_modify_book(client):
-    body = {"livro": "Java",
-            "escritor": "Igor"}
+    assert response.status_code == 201
+    assert "id" in response.json
+    assert "livro" in response.json
+    assert "escritor" in response.json
+
+
+def test_modify_book(client, mocker):
+    body = {"livro": "any_value",
+            "escritor": "any_value"}
+    modify_book_mock = mocker.MagicMock()
+    mocker.patch("controller.book_controller.modify_book", modify_book_mock)
+
     response = client.put("/book/1", json=body)
     assert response.status_code == 200
 
-def test_delete_book(client):
+
+def test_delete_book(client, mocker):
+    delete_book_mock = mocker.MagicMock()
+    mocker.patch("controller.book_controller.delete_book", delete_book_mock)
+
     response = client.delete("/book/1")
+
     assert response.status_code == 200
+    assert "mensagem" in response.json
--- tests/utils/create_book.py
+++ tests/utils/create_book.py
@@ -1,19 +1,26 @@
 def create_valid_book():
-    return {
+    return [{
         "id": 0,
-        "livro": "string",
-        "escritor": "string"
-    }
+        "livro": "any_value",
+        "escritor": "any_value"
+    }]
 
 def create_valid_list_book():
     return [{
         "id": 0,
-        "livro": "string",
-        "escritor": "string"
+        "livro": "any_value",
+        "escritor": "any_value"
     },
     {
         "id": 1,
-        "livro": "string",
-        "escritor": "string"
+        "livro": "any_value",
+        "escritor": "any_value"
     }]
+
+def create_response_successful_generate():
+    return {
+    "id": "any_value",
+    "livro": "any_value",
+    "escritor": "any_value"
+    }
 
