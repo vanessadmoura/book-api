bd1349d67d1eacb5e09d3fea999db12d875e1a67
vanessadmoura <nessa.moura8@gmail.com>
2023-02-21 15:58:29 -0300

    Fixed test get_by_id book

--- tests/test_controller/test_book_controller.py
+++ tests/test_controller/test_book_controller.py
@@ -18,22 +18,24 @@ def test_get_book_by_id_when_successfully(client, mocker):
     response = client.get("/api/v1/book/1")
 
     assert response.status_code == 200
-    assert len(response.json) == 1
+    assert "id" in response.json
+    assert "livro" in response.json
+    assert "escritor" in response.json
 
 
 def test_get_book_by_id_when_id_not_exits(client, mocker):
     list_book_mock = mocker.MagicMock(return_value=[])
     mocker.patch("controller.book_controller.get_book_by_id", list_book_mock)
 
-    response = client.get("/book/99")
+    response = client.get("/api/v1/book/99")
 
     assert response.status_code == 200
     assert len(response.json) == 0
 
 
 def test_register_book_when_successfully(client, mocker):
-    body = {"livro": "any_value",
-            "escritor": "any_value"}
+    body = {"livro": "anyvalue",
+            "escritor": "anyvalue"}
     register_book_mock = mocker.MagicMock()
     mocker.patch("controller.book_controller.register_book", register_book_mock)
     
@@ -46,12 +48,12 @@ def test_register_book_when_successfully(client, mocker):
 
 
 def test_modify_book_when_successfully(client, mocker):
-    body = {"livro": "any_value",
-            "escritor": "any_value"}
+    body = {"livro": "anyvalue",
+            "escritor": "anyvalue"}
     modify_book_response_mock = mocker.MagicMock(return_value=create_response_successful_generate())
     mocker.patch("controller.book_controller.modify_book", modify_book_response_mock)
 
-    response = client.put("/book/1", json=body)
+    response = client.put("/api/v1/book/1", json=body)
     assert response.status_code == 200
 
 
@@ -59,7 +61,7 @@ def test_delete_book_when_successfully(client, mocker):
     delete_book_mock = mocker.MagicMock()
     mocker.patch("controller.book_controller.delete_book", delete_book_mock)
 
-    response = client.delete("/book/1")
+    response = client.delete("/api/v1/book/1")
 
     assert response.status_code == 200
     assert "mensagem" in response.json
--- tests/utils/create_book.py
+++ tests/utils/create_book.py
@@ -1,20 +1,20 @@
 def create_valid_book():
-    return [{
-        "id": 0,
-        "livro": "any_value",
-        "escritor": "any_value"
-    }]
+    return {
+        "id": 1,
+        "livro": "anyvalue",
+        "escritor": "anyvalue"
+    }
 
 def create_valid_list_book():
     return [{
         "id": 0,
-        "livro": "any_value",
-        "escritor": "any_value"
+        "livro": "anyvalue",
+        "escritor": "anyvalue"
     },
     {
         "id": 1,
-        "livro": "any_value",
-        "escritor": "any_value"
+        "livro": "anyvalue",
+        "escritor": "anyvalue"
     }]
 
 def create_response_successful_generate():
